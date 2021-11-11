dcb41dbace598ab7d3e0a3998b34248c7984333b
carlos1433.92@gmail.com <JcnKRgnsMDO2nMUX5zJ1>
2021-11-11 01:02:53 -0300

    Login check, com $_SESSION['userName'] (Nome usuário)

--- src/loginCheck.php
+++ src/loginCheck.php
@@ -5,21 +5,22 @@
     if(empty($_POST['loginField']) || empty($_POST['pwField'])){
         header('Location: index.php');
         exit();
-    }
+    }   
 
-    $login = mysqli_real_escape_string($connection, $_POST['loginField']);
-    $pw = mysqli_real_escape_string($connection, $_POST['pwField']);
-    #$name = mysqli_query($connection, "SELECT `user_name` FROM `users` WHERE `user_login` = '{$login}' AND `user_pw` = '{$pw}'");
-    #$name_display = $name->fetch_row(0);
+    $login = mysqli_real_escape_string($connection, trim($_POST['loginField']));
+    $pw = mysqli_real_escape_string($connection, trim($_POST['pwField']));
+    $name = mysqli_query($connection, "SELECT `user_name` FROM `users` WHERE `user_login` = '{$login}' AND `user_pw` = md5('{$pw}')");
+    $name_display = $name->fetch_row();
     
 
-    $query = "SELECT userID, user_login FROM users WHERE user_login = '{$login}' AND user_pw = '{$pw}'";
+    $query = "SELECT userID, user_login FROM users WHERE user_login = '$login' AND user_pw = md5('$pw')";
 
-    $result = mysqli_query($connection,$query);
+    $result = mysqli_query($connection, $query);
     $row = mysqli_num_rows($result);
 
     if($row == 1){
         $_SESSION['loginField'] = $login;
+        $_SESSION['userName'] = $name_display[0];
         header('Location: mainPage.php');
         exit();
     }else{
