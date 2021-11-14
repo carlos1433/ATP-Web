ee4ae0df1387285bc620c2f4371debcba81bacd3
carlos1433.92@gmail.com <JcnKRgnsMDO2nMUX5zJ1>
2021-11-13 23:51:21 -0300

    Script para alteração de senha

--- src/changePassword.php
+++ src/changePassword.php
@@ -6,11 +6,23 @@
     $pw = mysqli_real_escape_string($connection, trim(md5($_POST['currentPw'])));
     $new_pw = mysqli_real_escape_string($connection, trim(md5($_POST['newPw'])));
     $new_pw2 = mysqli_real_escape_string($connection, trim(md5($_POST['newPwSameCheck'])));
+    $login = $_SESSION['loginField'];
 
-    if($new_pw != $new_pw2){
+    $sql = "SELECT user_pw FROM users WHERE user_login = '$login'";
+    $result = mysqli_query($connection, $sql);
+    $db_pw = mysqli_fetch_assoc($result);
+
+    if($pw != $db_pw['user_pw'] || $new_pw != $new_pw2){
         $_SESSION['pwIsDifferent'] = true;
         header('Location: myAccount.php');
         exit();
+    }else{
+        $sql = "UPDATE users SET user_pw = '$new_pw' WHERE user_login = '$login'";
+        if($connection->query($sql) === TRUE){
+            $_SESSION['updatedPw'] = true;
+            header('Location: myAccount.php');
+            exit();
+        }
     }
     
 
