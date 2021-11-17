5d859af1238777ce859756c154f113dcc2d51b6a
carlos1433.92@gmail.com <JcnKRgnsMDO2nMUX5zJ1>
2021-11-16 23:23:52 -0300

    Login check script

--- src/loginCheck.php
+++ src/loginCheck.php
@@ -16,11 +16,13 @@
     $query = "SELECT userID, user_login FROM users WHERE user_login = '$login' AND user_pw = md5('$pw')";
 
     $result = mysqli_query($connection, $query);
+    $id = mysqli_fetch_assoc($result)['userID'];
     $row = mysqli_num_rows($result);
 
     if($row == 1){
         $_SESSION['loginField'] = $login;
         $_SESSION['userName'] = $name_display[0];
+        $_SESSION['userID'] = $id;
         header('Location: mainPage.php');
         exit();
     }else{
