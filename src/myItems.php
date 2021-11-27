34d28cf20d606e2edcd0408877cc9cde041cbf67
carlos1433.92@gmail.com <JcnKRgnsMDO2nMUX5zJ1>
2021-11-27 01:56:12 -0300

    My Items page

--- src/myItems.php
+++ src/myItems.php
@@ -1,24 +1,28 @@
 <?php
-    session_start();
-    include('connect.php');
-    include('session_check.php');
-    $id = $_SESSION['userID'];
-    
+session_start();
+include('connect.php');
+include('session_check.php');
+$id = $_SESSION['userID'];
 
-    $sql = "SELECT COUNT(*) AS ITEMS FROM items WHERE item_owner_ID = $id";
-    $result = mysqli_query($connection, $sql);
-    $items_total = mysqli_fetch_assoc($result)["ITEMS"];
-    $_SESSION['items_total'] = $items_total;
 
-    $sql = "SELECT item_name, item_loan_date, item_loan_contact, item_return_date, item_returned FROM items WHERE item_owner_ID = $id ORDER BY item_loan_date";
-    $result = mysqli_query($connection, $sql);
-    $items_data = mysqli_fetch_all($result);
-    $_SESSION['items_data'] = $items_data; 
+$sql = "SELECT COUNT(*) AS ITEMS FROM items WHERE item_owner_ID = $id";
+$result = mysqli_query($connection, $sql);
+$items_total = mysqli_fetch_assoc($result)["ITEMS"];
+$_SESSION['items_total'] = $items_total;
+
+$sql = "SELECT item_ID, item_name, item_loan_date, item_loan_contact, item_return_date, item_returned 
+            FROM items 
+            WHERE item_owner_ID = $id 
+            ORDER BY item_returned, item_loan_date";
+$result = mysqli_query($connection, $sql);
+$items_data = mysqli_fetch_all($result);
+$_SESSION['items_data'] = $items_data;
 
 ?>
 
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -26,8 +30,9 @@
     <link rel="shortcut icon" href="/imgs/ca.ico" type="image/x-icon">
     <title>userName - Coisas Emprestadas</title>
 </head>
+
 <body>
-    <form action="#">
+    <form action="myItemsUpdate.php" method="post">
         <div class="myItemsTableContainer">
             <table>
                 <tr>
@@ -37,24 +42,25 @@
                     <th>Data Devolução</th>
                     <th>Devolvido</th>
                 </tr>
-                <?php 
-                for ($i = 0; $i < $items_total; $i++){
+                <?php
+                for ($i = 0; $i < $items_total; $i++) {
+                    echo '<tr>';
+
+                    for ($j = 1; $j < 6; $j++) {
+                        if ($items_data[$i][$j] === null) {
+                            echo '<td>Item não devolvido</td>';
+                        } else if ($items_data[$i][$j] == 0) {
+                            echo sprintf('<td><input type="checkbox" value="%s" name="isReturned[]"></td>', $items_data[$i][0]);
+                        } else if ($items_data[$i][$j] == 1) {
+                            echo '<td>Item devolvido!</td>';
+                        } else {
+                            echo sprintf('<td>%s</td>', $items_data[$i][$j]);
+                        }
+                    }
+                    echo '</tr>';
+                }
                 ?>
-                <tr>
-                <?php 
-                    for ($j = 0; $j < 5; $j++){
-                        if($items_data[$i][$j] === null){ ?>
-                            <td><input type="date"></td>
-                        <?php }else if($items_data[$i][$j] == 0){ ?>
-                            <td><input type="checkbox" value="1" name="isReturned"></td>
-                        <?php }else if($items_data[$i][$j] == 1){ ?>
-                            <td>Item devolvido!</td>
-                        <?php }else{ ?>
-                            <td><?php echo $items_data[$i][$j] ?></td>
-                        <?php } ?>
-                    <?php } ?>
-                <?php } ?>
-                </tr>
+
             </table>
         </div>
         <div class="buttonWrapper">
@@ -62,7 +68,7 @@
                 <a href="mainPage.php" class="buttonLink">Voltar</a>
             </button>
             <button class="saveButton" type="submit">
-                <a href="#" class="buttonLink">Salvar</a>
+                Salvar
             </button>
             <button class="registerItemButton">
                 <a href="registerItem.php" class="buttonLink">Cadastrar Item</a>
@@ -70,4 +76,5 @@
         </div>
     </form>
 </body>
+
 </html>
